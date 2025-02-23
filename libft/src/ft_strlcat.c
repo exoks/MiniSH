3387f1da6f35afe493eef935a94160ef6ba9d1a0
Oussama Ezzaou <oezzaou@c2r1p9.1337.ma>
2025-02-23 20:44:24 +0000

    minish: libft: ft_strlen & ft_strlcat are updated

--- libft/src/ft_strlcat.c
+++ libft/src/ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: oezzaou <oezzaou@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/29 21:23:49 by oezzaou           #+#    #+#             */
-/*   Updated: 2022/10/10 13:10:33 by oezzaou          ###   ########.fr       */
+/*   Updated: 2025/02/23 20:34:53 by oezzaou          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
@@ -14,19 +14,19 @@
 //====<[ ft_strlcat: ]>=========================================================
 size_t	ft_strlcat(char	*dst, const char *src, size_t dstsize)
 {
-	size_t	i;
+	size_t	index;
 	size_t	dst_len;
 	size_t	src_len;
 
-	if (dst == 0 && dstsize == 0)
+	if (src == 0 || dstsize == 0)
 		return (ft_strlen(src));
 	dst_len = ft_strlen(dst);
 	src_len = ft_strlen(src);
 	if (dstsize <= dst_len)
 		return (src_len + dstsize);
-	i = dst_len;
-	while (*src && i < dstsize - 1)
-		dst[i++] = *(src++);
-	dst[i] = 0;
+	index = dst_len;
+	while (*src && index < dstsize - 1)
+		dst[index++] = *(src++);
+	dst[index] = 0;
 	return (dst_len + src_len);
 }
